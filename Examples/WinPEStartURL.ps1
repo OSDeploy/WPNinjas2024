7a515cbb4fd45ffa1b577035b254d03ff82f2ece
David Segura <david@segura.org>
2024-09-18 18:12:13 +0200

    Update WinPEStartURL.ps1

--- Examples/WinPEStartURL.ps1
+++ Examples/WinPEStartURL.ps1
@@ -2,10 +2,20 @@
 # Edit-OSDCloudWinPE -StartURL "https://raw.githubusercontent.com/OSDeploy/WPNinjas2024/main/Examples/WinPEStartURL.ps1"
 
 Write-Host "Pre OSDCloud" -ForegroundColor Cyan
-# Add something to do here
+# Set the MyOSDCloud Global Variable
+$Global:MyOSDCloud = @{
+    ClearDiskConfirm = $false
+    Debug = $true
+    MSCatalogFirmware = $false
+    MSCatalogDiskDrivers = $true
+    MSCatalogNetDrivers = $true
+    MSCatalogScsiDrivers = $false
+    Restart = $true
+    WindowsDefenderUpdate = $true
+}
 
 Write-Host 'OSDCloud' -ForegroundColor Cyan
 Start-OSDCloud -OSName 'Windows 11 23H2 x64' -OSEdition Enterprise -OSLanguage en-us
 
 Write-Host 'Post OSDCloud' -ForegroundColor Cyan
-Restart-Computer
+# Do something after OSDCloud
